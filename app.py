54b90ad28e0c46b1cf08bf29742aebc77f9509e7
susanap8a <sperea@uach.mx>
2024-11-23 13:45:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.balloons()
 # Show title and description.
 st.title("💬 mi app")
 st.write(
-   "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
+   "Este es un chat de prueba para accesar a respuestas limitadas. "
    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )

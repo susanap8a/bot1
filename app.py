49d75c22733bf2414618588693304959aea3d737
demo-taller-uach <demo.taller.uach@gmail.com>
2024-11-22 21:32:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.write(
    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_api_key = st.secrets[api_key] #st.text_input("OpenAI API Key", type="password")
 #if not openai_api_key:
 #   st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 #else:
